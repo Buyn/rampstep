4c5d2703634f7673932b4871b55e41b0f4c2abd7
BuYn <buyn.tex@gmail.com>
2018-08-30 13:21:34 +0300

    Commit for mish

--- Source/RAMPS_StepMotor/RAMPS_StepMotor.ino
+++ Source/RAMPS_StepMotor/RAMPS_StepMotor.ino
@@ -12,6 +12,45 @@
   by Scott Fitzgerald
  *}}}*/
 /*   Constants   *{{{
+#define X_STEP_PIN         10
+#define X_DIR_PIN          12
+#define X_ENABLE_PIN       38
+#define X_MIN_PIN           3
+#define X_MAX_PIN           2
+
+#define Y_STEP_PIN         60
+#define Y_DIR_PIN          61
+#define Y_ENABLE_PIN       56
+#define Y_MIN_PIN          14
+#define Y_MAX_PIN          15
+
+#define Z_STEP_PIN         46
+#define Z_DIR_PIN          48
+#define Z_ENABLE_PIN       62
+#define Z_MIN_PIN          18
+#define Z_MAX_PIN          19
+
+#define E_STEP_PIN         26
+#define E_DIR_PIN          28
+#define E_ENABLE_PIN       24
+
+#define Q_STEP_PIN         36
+#define Q_DIR_PIN          34
+#define Q_ENABLE_PIN       30
+
+#define SDPOWER            -1
+#define SDSS               53
+#define LED_PIN            13
+
+#define FAN_PIN            9
+
+#define PS_ON_PIN          12
+#define KILL_PIN           -1
+
+#define HEATER_0_PIN       10
+#define HEATER_1_PIN       8
+#define TEMP_0_PIN          13   // ANALOG NUMBERING
+#define TEMP_1_PIN          14   // ANALOG NUMBERING
 //}}}*/
 /*Global Values {{{*/
 int sensorValue =100;
@@ -35,9 +74,7 @@ void loop() {
   //Serial.println(sensorValue);
   sensorValue = analogRead(A3);
   analogWrite(A2, sensorValue);
-  //analogWrite(A10, 500);
   analogWrite(A5, 3000);
-  //analogWrite(A15, 3500);
   if (digitalRead(11)>LOW){
     digitalWrite(12, LOW);
   }else{
@@ -45,7 +82,6 @@ void loop() {
   }
   delayms=sensorValue/1;
   delay(delayms);              // wait for a second
-  //delay(40);
   step(10);
 }/*}}}*/
 /* Step function  {{{
